abd49262b32f1292f8958e5fd19b3fb18f51b722
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-20 06:09:37 +0300

    Изменения в conftest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -16,7 +16,6 @@ from app.db.models import Base
 
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
-encoded_password = quote_plus(POSTGRES_PASSWORD)
 TEST_DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/test_db"
 
 
